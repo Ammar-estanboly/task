edcff391bc766225a0ae2f4596f91097ddadd6b1
Ammar-estanboly <ammarestanboly111@gamil.com>
2022-11-22 07:08:54 +0200

    add dashboard files

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::get('register', [AdminController::class, 'index']);
+
+Route::get('/{page}', [AdminController::class, 'index']);
